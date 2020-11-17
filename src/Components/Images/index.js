0a89b68a5c2d9ba0bc05111a27eed60dcf972f99
Andra Maxim <60080140+AndraMaxim@users.noreply.github.com>
2020-11-17 11:01:16 +0200

    Add files via upload

---
+++ src/Components/Images/index.js
@@ -0,0 +1,73 @@
+
+import aiai from "./aiai.png";
+import dodo from "./dodo.png";
+import gigi from "./gigi.png";
+import mitzi from "./mitzi.png";
+import momo from "./momo.png";
+import roro from "./roro.png";
+import card from "./card.png";
+
+export let images = [
+    {
+        name: "aiai",
+        pic: aiai,
+        flipped: false,
+    },
+    {
+        name: "dodo",
+        pic: dodo,
+        flipped: false,
+    },
+    {
+        name: "gigi",
+        pic: gigi,
+        flipped: false,
+    },
+    {
+        name: "mitzi",
+        pic: mitzi,
+        flipped: false,
+    },
+    {
+        name: "momo",
+        pic: momo,
+        flipped: false,
+    },
+    {
+        name: "roro",
+        pic: roro,
+        flipped: false,
+    },
+    {
+        name: "aiai",
+        pic: aiai,
+        flipped: false,
+    },
+    {
+        name: "dodo",
+        pic: dodo,
+        flipped: false,
+    },
+    {
+        name: "gigi",
+        pic: gigi,
+        flipped: false,
+    },
+    {
+        name: "mitzi",
+        pic: mitzi,
+        flipped: false,
+    },
+    {
+        name: "momo",
+        pic: momo,
+        flipped: false,
+    },
+    {
+        name: "roro",
+        pic: roro,
+        flipped: false,
+    },
+];
+
+export const blankCard = card;
